0bb1ad20a5cc521630cfec263ce5f1dd4b720380
valentinaivanova1910 <125904818+valentinaivanova1910@users.noreply.github.com>
2023-05-16 18:27:17 +0300

    Add project files.

---
+++ LessonOOPExampleProject/human.cpp
@@ -0,0 +1,81 @@
+#include "student.h"
+
+
+
+// default constructor
+Human::Human() {
+	name = "no name";
+	surname = "no surname";
+	age = 15;
+}
+
+// constructor with arguments
+Human::Human(string nm, string surnm, int a ) {
+	
+	//cout << "constructor with arguments" << endl;
+	name = nm;
+	surname = surnm;
+	age = a;
+	
+}
+
+
+
+
+Human::~Human() {
+	count--;
+	//cout << "destructor" << endl;
+}
+
+string Student::getName() {
+	return name;
+}
+
+void Student::setName(string n) {
+	name = n;
+}
+
+string Student::getSurname() {
+	return surname;
+}
+
+void Student::setSurname(string n) {
+	surname = n;
+}
+
+int Student::getAge() {
+	return age;
+}
+
+void Student::setAge(int a) {
+	if (a > 0 && a < 110) {
+		age = a;
+	}
+}
+
+float Student::getAvgMark() {
+	return avg_mark;
+}
+
+void Student::setAvgMark(float mark) {
+	if (mark >= 0 && mark <= 10) {
+		avg_mark = mark;
+	}
+}
+
+void Student::clear() {
+	name = "no name";
+	surname = "no surname";
+	age = 0;
+	avg_mark = 0;
+}
+
+string Student::convert() {
+	string msg = "";
+	msg += name;
+	msg += " " + surname;
+	msg += " ( age = " + to_string(age);
+	msg += ", average mark = " + to_string(avg_mark);
+	msg += ")";
+	return msg;
+}
